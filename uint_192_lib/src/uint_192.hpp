2b8ae7b79270a7e4a3c93bb2f1d62e46338c8df7
John Sorial <john.sorial@pickware.de>
2023-12-03 14:10:10 +0100

    Feat: Add left shift functionality

--- uint_192_lib/src/uint_192.hpp
+++ uint_192_lib/src/uint_192.hpp
@@ -8,21 +8,47 @@
 #define MY_UINT192_UINT_192_HPP
 
 namespace uint192_lib {
-    struct uint_192 {
-        std::array<std::uint64_t, 3> parts;
-    };
+struct uint_192 {
+    std::array<std::uint64_t, 3> parts;
+};
 
-    constexpr uint_192 operator+(const uint_192 &lhs, const uint_192 &rhs) noexcept {
-        uint_192 result{};
-        uint64_t carry_bit{0};
-        for (uint64_t i = 0; i < lhs.parts.size(); ++i) {
-            result.parts[i] += carry_bit;
-            result.parts[i] += lhs.parts[i] + rhs.parts[i];
-            carry_bit = result.parts[i] < lhs.parts[i];
+constexpr uint_192 operator+(const uint_192 &lhs, const uint_192 &rhs) noexcept {
+    uint_192 result{};
+    uint64_t carry_bit{0};
+
+    for (uint64_t i = 0; i < lhs.parts.size(); ++i) {
+        result.parts[i] += carry_bit;
+        result.parts[i] += lhs.parts[i] + rhs.parts[i];
+        carry_bit = result.parts[i] < lhs.parts[i];
+    }
+    return result;
+}
+
+constexpr uint_192 operator<<(const uint_192 &number, const uint64_t shift) noexcept {
+    uint_192 result{};
+    uint64_t carry{};
+
+    if (shift < UINT64_WIDTH) {
+        for (uint64_t current_position = 0; current_position < number.parts.size();
+             ++current_position) {
+            result.parts[current_position] += carry;
+            carry = number.parts[current_position] >> (UINT64_WIDTH - shift);
+            uint64_t shifted = number.parts[current_position] << shift;
+            result.parts[current_position] += shifted;
         }
-        return result;
+
+    } else {
+        const uint64_t shift_adjust = shift % UINT64_WIDTH;
+        const uint64_t shift_offset = shift / UINT64_WIDTH;
+        for (uint64_t current_position = shift_offset; current_position < number.parts.size();
+             ++current_position) {
+            const uint64_t shift_from_index = current_position - shift_offset;
+            result.parts[current_position] = number.parts[shift_from_index] << shift_adjust;
+        }
     }
 
+    return result;
+}
 
 } // namespace uint192_lib
 
---
+++ uint_192_lib/tests/test_left_shifting.cpp
@@ -0,0 +1,244 @@
+//
+// Created by jn98zk on 26.11.23.
+//
+
+#include <catch2/benchmark/catch_benchmark.hpp>
+#include <catch2/catch_test_macros.hpp>
+#include <uint_192.hpp>
+
+SCENARIO("Shifting works") {
+
+    GIVEN("A uint_192 of value {1,0,0}") {
+        uint192_lib::uint_192 a{1};
+
+        THEN("Shifting it by 1 bit works") {
+            auto result = a << 1;
+            REQUIRE(result.parts[0] == 2);
+            REQUIRE(result.parts[1] == 0);
+            REQUIRE(result.parts[2] == 0);
+        }
+
+        THEN("Shifting it by 2 bits works") {
+            auto result = a << 2;
+            REQUIRE(result.parts[0] == 4);
+            REQUIRE(result.parts[1] == 0);
+            REQUIRE(result.parts[2] == 0);
+        }
+
+        THEN("Shifting it by 10 bits works") {
+            auto result = a << 10;
+            REQUIRE(result.parts[0] == 1 << 10);
+            REQUIRE(result.parts[1] == 0);
+            REQUIRE(result.parts[2] == 0);
+        }
+
+        THEN("Shifting it by 64 bits works") {
+            auto result = a << UINT64_WIDTH;
+            REQUIRE(result.parts[0] == 0);
+            REQUIRE(result.parts[1] == 1);
+            REQUIRE(result.parts[2] == 0);
+        }
+
+        THEN("Shifting it by 128 bits works") {
+            auto result = a << UINT64_WIDTH * 2;
+            REQUIRE(result.parts[0] == 0);
+            REQUIRE(result.parts[1] == 0);
+            REQUIRE(result.parts[2] == 1);
+        }
+    }
+
+    GIVEN("A uint_192 of value {0,1,0}") {
+        uint192_lib::uint_192 a{0, 1};
+
+        THEN("Shifting it by 1 bit works") {
+            auto result = a << 1;
+            REQUIRE(result.parts[0] == 0);
+            REQUIRE(result.parts[1] == 2);
+            REQUIRE(result.parts[2] == 0);
+        }
+
+        THEN("Shifting it by 2 bits works") {
+            auto result = a << 2;
+            REQUIRE(result.parts[0] == 0);
+            REQUIRE(result.parts[1] == 4);
+            REQUIRE(result.parts[2] == 0);
+        }
+
+        THEN("Shifting it by 10 bits works") {
+            auto result = a << 10;
+            REQUIRE(result.parts[0] == 0);
+            REQUIRE(result.parts[1] == 1 << 10);
+            REQUIRE(result.parts[2] == 0);
+        }
+
+        THEN("Shifting it by 64 bits works") {
+            auto result = a << UINT64_WIDTH;
+            REQUIRE(result.parts[0] == 0);
+            REQUIRE(result.parts[1] == 0);
+            REQUIRE(result.parts[2] == 1);
+        }
+
+        THEN("Shifting it by 128 bit returns an empty result") {
+            auto result = a << UINT64_WIDTH * 2;
+            REQUIRE(result.parts[0] == 0);
+            REQUIRE(result.parts[1] == 0);
+            REQUIRE(result.parts[2] == 0);
+        }
+    }
+
+    GIVEN("A uint_192 of value {0,0,1}") {
+        uint192_lib::uint_192 a{0, 0, 1};
+
+        THEN("Shifting it by 1 bit works") {
+            auto result = a << 1;
+            REQUIRE(result.parts[0] == 0);
+            REQUIRE(result.parts[1] == 0);
+            REQUIRE(result.parts[2] == 2);
+        }
+
+        THEN("Shifting it by 2 bits works") {
+            auto result = a << 2;
+            REQUIRE(result.parts[0] == 0);
+            REQUIRE(result.parts[1] == 0);
+            REQUIRE(result.parts[2] == 4);
+        }
+
+        THEN("Shifting it by 10 bits works") {
+            auto result = a << 10;
+            REQUIRE(result.parts[0] == 0);
+            REQUIRE(result.parts[1] == 0);
+            REQUIRE(result.parts[2] == 1 << 10);
+        }
+
+        THEN("Shifting it by 64 bits returns an empty result") {
+            auto result = a << UINT64_WIDTH;
+            REQUIRE(result.parts[0] == 0);
+            REQUIRE(result.parts[1] == 0);
+            REQUIRE(result.parts[2] == 0);
+        }
+
+        THEN("Shifting it by 128 bit returns an empty result") {
+            auto result = a << UINT64_WIDTH * 2;
+            REQUIRE(result.parts[0] == 0);
+            REQUIRE(result.parts[1] == 0);
+            REQUIRE(result.parts[2] == 0);
+        }
+    }
+
+    GIVEN("A uint_192 of value {1,1,0}") {
+        uint192_lib::uint_192 a{1, 1, 0};
+
+        THEN("Shifting it by 1 bit works") {
+            auto result = a << 1;
+            REQUIRE(result.parts[0] == 2);
+            REQUIRE(result.parts[1] == 2);
+            REQUIRE(result.parts[2] == 0);
+        }
+
+        THEN("Shifting it by 2 bits works") {
+            auto result = a << 2;
+            REQUIRE(result.parts[0] == 4);
+            REQUIRE(result.parts[1] == 4);
+            REQUIRE(result.parts[2] == 0);
+        }
+
+        THEN("Shifting it by 10 bits works") {
+            auto result = a << 10;
+            REQUIRE(result.parts[0] == 1 << 10);
+            REQUIRE(result.parts[1] == 1 << 10);
+            REQUIRE(result.parts[2] == 0);
+        }
+
+        THEN("Shifting it by 64 bit works") {
+            auto result = a << UINT64_WIDTH;
+            REQUIRE(result.parts[0] == 0);
+            REQUIRE(result.parts[1] == 1);
+            REQUIRE(result.parts[2] == 1);
+        }
+
+        THEN("Shifting it by 128 bit overflows correctly") {
+            auto result = a << UINT64_WIDTH * 2;
+            REQUIRE(result.parts[0] == 0);
+            REQUIRE(result.parts[1] == 0);
+            REQUIRE(result.parts[2] == 1);
+        }
+    }
+
+    GIVEN("A uint_192 of value {UINT64_MAX,0,0}") {
+        uint192_lib::uint_192 a{UINT64_MAX, 0, 0};
+
+        THEN("Shifting it by 1 bit works") {
+            auto result = a << 1;
+            REQUIRE(result.parts[0] == UINT64_MAX << 1);
+            REQUIRE(result.parts[1] == UINT64_MAX >> 63);
+            REQUIRE(result.parts[2] == 0);
+        }
+
+        THEN("Shifting it by 2 bits works") {
+            auto result = a << 2;
+            REQUIRE(result.parts[0] == UINT64_MAX << 2);
+            REQUIRE(result.parts[1] == UINT64_MAX >> 62);
+            REQUIRE(result.parts[2] == 0);
+        }
+
+        THEN("Shifting it by 10 bits works") {
+            auto result = a << 10;
+            REQUIRE(result.parts[0] == UINT64_MAX << 10);
+            REQUIRE(result.parts[1] == UINT64_MAX >> 54);
+            REQUIRE(result.parts[2] == 0);
+        }
+
+        THEN("Shifting it by 64 bit works") {
+            auto result = a << UINT64_WIDTH;
+            REQUIRE(result.parts[0] == 0);
+            REQUIRE(result.parts[1] == UINT64_MAX);
+            REQUIRE(result.parts[2] == 0);
+        }
+
+        THEN("Shifting it by 128 bit works correctly") {
+            auto result = a << UINT64_WIDTH * 2;
+            REQUIRE(result.parts[0] == 0);
+            REQUIRE(result.parts[1] == 0);
+            REQUIRE(result.parts[2] == UINT64_MAX);
+        }
+    }
+
+    GIVEN("A uint_192 of value {UINT64_MAX, UINT64_MAX,0}") {
+        uint192_lib::uint_192 a{UINT64_MAX, UINT64_MAX, 0};
+
+        THEN("Shifting it by 1 bit works") {
+            auto result = a << 1;
+            REQUIRE(result.parts[0] == UINT64_MAX << 1);
+            REQUIRE(result.parts[1] == UINT64_MAX);
+            REQUIRE(result.parts[2] == UINT64_MAX >> 63);
+        }
+
+        THEN("Shifting it by 2 bits works") {
+            auto result = a << 2;
+            REQUIRE(result.parts[0] == UINT64_MAX << 2);
+            REQUIRE(result.parts[1] == UINT64_MAX);
+            REQUIRE(result.parts[2] == UINT64_MAX >> 62);
+        }
+
+        THEN("Shifting it by 10 bits works") {
+            auto result = a << 10;
+            REQUIRE(result.parts[0] == UINT64_MAX << 10);
+            REQUIRE(result.parts[1] == UINT64_MAX);
+            REQUIRE(result.parts[2] == UINT64_MAX >> 54);
+        }
+
+        THEN("Shifting it by 64 bit works") {
+            auto result = a << UINT64_WIDTH;
+            REQUIRE(result.parts[0] == 0);
+            REQUIRE(result.parts[1] == UINT64_MAX);
+            REQUIRE(result.parts[2] == UINT64_MAX);
+        }
+
+        THEN("Shifting it by 128 bit works correctly") {
+            auto result = a << UINT64_WIDTH * 2;
+            REQUIRE(result.parts[0] == 0);
+            REQUIRE(result.parts[1] == 0);
+            REQUIRE(result.parts[2] == UINT64_MAX);
+        }
+    }
+}
