ffd44cfddf2e2675fab7456921b1cc5a96e1930f
John Sorial <john.sorial@pickware.de>
2023-12-08 23:06:24 +0100

    Fix

--- uint_192_lib/tests/test_comparison_operators.cpp
+++ uint_192_lib/tests/test_comparison_operators.cpp
@@ -57,112 +57,19 @@ SCENARIO("Test < operator", "[uint_192]") {
     GIVEN("A uint_192 of value {5,0,0}") {
         uint192_lib::uint_192 a{5};
 
-        THEN("It is not larger than another uint_192 of value {6,0,0}") {
-            uint192_lib::uint_192 b{6};
-            REQUIRE_FALSE(a > b);
-        }
-
-        THEN("It is not larger than another uint_192 of value {0,1,0}") {
-            uint192_lib::uint_192 b{0, 1};
-            REQUIRE_FALSE(a > b);
-        }
-
-        THEN("It is not larger than another uint_192 of value {0,0,1}") {
-            uint192_lib::uint_192 b{0, 0, 1};
-            REQUIRE_FALSE(a > b);
-        }
-
-        THEN("It is not larger than another uint_192 of value {0,0,0}") {
-            uint192_lib::uint_192 b{0};
-            REQUIRE_FALSE(a < b);
-        }
-
-        THEN("It is not larger than another uint_192 of value {5,0,0}") {
-            uint192_lib::uint_192 b{5};
-            REQUIRE_FALSE(a > b);
-        }
-    }
-
-    GIVEN("A uint_192 of value {0,1,0}") {
-        uint192_lib::uint_192 a{0, 1};
-
-        THEN("It is not larger than another uint_192 of value {0,2,0}") {
-            uint192_lib::uint_192 b{0, 2};
-            REQUIRE_FALSE(a > b);
-        }
-        THEN("It is not larger than another uint_192 of value {0,0,1}") {
-            uint192_lib::uint_192 b{0, 0, 1};
-            REQUIRE_FALSE(a > b);
-        }
-
-        THEN("It is not larger than another uint_192 of value {0,1,0}") {
-            uint192_lib::uint_192 b{0, 1};
-            REQUIRE_FALSE(a > b);
-        }
-
-        THEN("It is larger than another uint_192 of value {0,0,0}") {
-            uint192_lib::uint_192 b{0};
-            REQUIRE(a >= b);
-        }
-    }
-
-    GIVEN("A uint_192 of value {0,0,1}") {
-        uint192_lib::uint_192 a{0, 0, 1};
-
-        THEN("It is not larger than another uint_192 of value {0,0,2}") {
-            uint192_lib::uint_192 b{0, 0, 2};
-            REQUIRE_FALSE(a > b);
-        }
-
-        THEN("It is not larger than another uint_192 of value {0,0,3}") {
-            uint192_lib::uint_192 b{0, 0, 3};
-            REQUIRE_FALSE(a > b);
-        }
-
-        THEN("It is not larger than another uint_192 of value {0,0,UINT64_MAX}") {
-            uint192_lib::uint_192 b{0, 0, UINT64_MAX};
-            REQUIRE_FALSE(a > b);
-        }
-
-        THEN("It is not larger than another uint_192 of value {0,0,1}") {
-            uint192_lib::uint_192 b{0, 0, 1};
-            REQUIRE_FALSE(a > b);
-        }
-
-        THEN("It larger than another uint_192 of value {UINT64_MAX,0,0}") {
-            uint192_lib::uint_192 b{UINT64_MAX, 0, 0};
-            REQUIRE(a > b);
-        }
-
-        THEN("It larger than another uint_192 of value {UINT64_MAX,UINT64_MAX,0}") {
-            uint192_lib::uint_192 b{UINT64_MAX, UINT64_MAX, 0};
-            REQUIRE(a > b);
-        }
-
-        THEN("It is larger than another uint_192 of value {0,0,0}") {
-            uint192_lib::uint_192 b{0};
-            REQUIRE(a > b);
-        }
-    }
-}
-
-SCENARIO("Test > operator", "[uint_192]") {
-    GIVEN("A uint_192 of value {5,0,0}") {
-        uint192_lib::uint_192 a{5};
-
         THEN("It is smaller than another uint_192 of value {6,0,0}") {
             uint192_lib::uint_192 b{6};
-            REQUIRE_FALSE(a > b);
+            REQUIRE(a < b);
         }
 
         THEN("It is smaller than another uint_192 of value {0,1,0}") {
             uint192_lib::uint_192 b{0, 1};
-            REQUIRE_FALSE(a > b);
+            REQUIRE(a < b);
         }
 
         THEN("It is smaller than another uint_192 of value {0,0,1}") {
             uint192_lib::uint_192 b{0, 0, 1};
-            REQUIRE_FALSE(a > b);
+            REQUIRE(a < b);
         }
 
         THEN("It is smaller than another uint_192 of value {0,0,0}") {
@@ -181,16 +88,21 @@ SCENARIO("Test > operator", "[uint_192]") {
 
         THEN("It is smaller than another uint_192 of value {0,2,0}") {
             uint192_lib::uint_192 b{0, 2};
-            REQUIRE_FALSE(a > b);
+            REQUIRE(a < b);
         }
         THEN("It is smaller than another uint_192 of value {0,0,1}") {
             uint192_lib::uint_192 b{0, 0, 1};
-            REQUIRE_FALSE(a > b);
+            REQUIRE(a < b);
         }
 
         THEN("It is not smaller than another uint_192 of value {0,1,0}") {
             uint192_lib::uint_192 b{0, 1};
-            REQUIRE_FALSE(a > b);
+            REQUIRE_FALSE(a < b);
+        }
+
+        THEN("It not smaller than another uint_192 of value {0,0,0}") {
+            uint192_lib::uint_192 b{0};
+            REQUIRE_FALSE(a < b);
         }
     }
 
@@ -199,17 +111,17 @@ SCENARIO("Test > operator", "[uint_192]") {
 
         THEN("It is smaller than another uint_192 of value {0,0,2}") {
             uint192_lib::uint_192 b{0, 0, 2};
-            REQUIRE_FALSE(a > b);
+            REQUIRE(a < b);
         }
 
         THEN("It is smaller than another uint_192 of value {0,0,3}") {
             uint192_lib::uint_192 b{0, 0, 3};
-            REQUIRE_FALSE(a > b);
+            REQUIRE(a < b);
         }
 
         THEN("It is smaller than another uint_192 of value {0,0,UINT64_MAX}") {
             uint192_lib::uint_192 b{0, 0, UINT64_MAX};
-            REQUIRE_FALSE(a > b);
+            REQUIRE(a < b);
         }
 
         THEN("It is not smaller than another uint_192 of value {0,0,1}") {
@@ -230,6 +142,94 @@ SCENARIO("Test > operator", "[uint_192]") {
         THEN("It is not smaller than another uint_192 of value {0,0,0}") {
             uint192_lib::uint_192 b{0};
             REQUIRE_FALSE(a < b);
+        }
+    }
+}
+
+SCENARIO("Test > operator", "[uint_192]") {
+    GIVEN("A uint_192 of value {5,0,0}") {
+        uint192_lib::uint_192 a{5};
+
+        THEN("It not larger than another uint_192 of value {6,0,0}") {
+            uint192_lib::uint_192 b{6};
+            REQUIRE_FALSE(a > b);
+        }
+
+        THEN("It not larger than another uint_192 of value {0,1,0}") {
+            uint192_lib::uint_192 b{0, 1};
+            REQUIRE_FALSE(a > b);
+        }
+
+        THEN("It not larger than another uint_192 of value {0,0,1}") {
+            uint192_lib::uint_192 b{0, 0, 1};
+            REQUIRE_FALSE(a > b);
+        }
+
+        THEN("It not larger than another uint_192 of value {0,0,0}") {
+            uint192_lib::uint_192 b{0};
+            REQUIRE(a > b);
+        }
+
+        THEN("It is not smaller than another uint_192 of value {5,0,0}") {
+            uint192_lib::uint_192 b{5};
+            REQUIRE_FALSE(a > b);
+        }
+    }
+
+    GIVEN("A uint_192 of value {0,1,0}") {
+        uint192_lib::uint_192 a{0, 1};
+
+        THEN("It not larger than another uint_192 of value {0,2,0}") {
+            uint192_lib::uint_192 b{0, 2};
+            REQUIRE_FALSE(a > b);
+        }
+        THEN("It not larger than another uint_192 of value {0,0,1}") {
+            uint192_lib::uint_192 b{0, 0, 1};
+            REQUIRE_FALSE(a > b);
+        }
+
+        THEN("It is not smaller than another uint_192 of value {0,1,0}") {
+            uint192_lib::uint_192 b{0, 1};
+            REQUIRE_FALSE(a > b);
+        }
+    }
+
+    GIVEN("A uint_192 of value {0,0,1}") {
+        uint192_lib::uint_192 a{0, 0, 1};
+
+        THEN("It not larger than than another uint_192 of value {0,0,2}") {
+            uint192_lib::uint_192 b{0, 0, 2};
+            REQUIRE_FALSE(a > b);
+        }
+
+        THEN("It not larger than than another uint_192 of value {0,0,3}") {
+            uint192_lib::uint_192 b{0, 0, 3};
+            REQUIRE_FALSE(a > b);
+        }
+
+        THEN("It not larger than than another uint_192 of value {0,0,UINT64_MAX}") {
+            uint192_lib::uint_192 b{0, 0, UINT64_MAX};
+            REQUIRE_FALSE(a > b);
+        }
+
+        THEN("It is not larger than another uint_192 of value {0,0,1}") {
+            uint192_lib::uint_192 b{0, 0, 1};
+            REQUIRE_FALSE(a > b);
+        }
+
+        THEN("It is larger than another uint_192 of value {UINT64_MAX,0,0}") {
+            uint192_lib::uint_192 b{UINT64_MAX, 0, 0};
+            REQUIRE(a > b);
+        }
+
+        THEN("It is larger than another uint_192 of value {UINT64_MAX,UINT64_MAX,0}") {
+            uint192_lib::uint_192 b{UINT64_MAX, UINT64_MAX, 0};
+            REQUIRE(a > b);
+        }
+
+        THEN("It is larger than another uint_192 of value {0,0,0}") {
+            uint192_lib::uint_192 b{0};
+            REQUIRE(a > b);
         }
     }
 }
