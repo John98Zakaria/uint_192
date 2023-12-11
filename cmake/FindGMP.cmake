1030ecb4675c8b4de4eac47b9538cc3f7d925ecf
John Sorial <johnzakaria98@gmail.com>
2023-12-12 00:02:57 +0100

    fix: Include C lib when finding GMP

--- cmake/FindGMP.cmake
+++ cmake/FindGMP.cmake
@@ -2,6 +2,8 @@
 
 # Designed to included in the CMAKE_MODULE_PATH and called using find_package
 # Inspired by https://dominikberner.ch/cmake-find-library/
+# Tested on linux only
+
 include(GNUInstallDirs)
 
 find_library(
@@ -12,9 +14,22 @@ find_library(
 )
 
 find_library(
+        GMP_C_SHARED_LIBRARY
+        GMP
+        NAMES libgmp.so
+        PATH_SUFFIXES lib lib64
+)
+
+find_library(
         GMP_STATIC_LIBRARY
         GMP
         NAMES libgmpxx.a
+        PATH_SUFFIXES lib lib64
+)
+find_library(
+        GMP_C_STATIC_LIBRARY
+        GMP
+        NAMES libgmp.a
         PATH_SUFFIXES lib lib64
 )
 
@@ -28,7 +43,9 @@ include(FindPackageHandleStandardArgs)
 
 find_package_handle_standard_args(GMP DEFAULT_MSG
         GMP_SHARED_LIBRARY
+        GMP_C_SHARED_LIBRARY
         GMP_STATIC_LIBRARY
+        GMP_C_STATIC_LIBRARY
         GMP_INCLUDE_DIR)
 
 mark_as_advanced(GMP_LIBRARY GMP_INCLUDE_DIR)
@@ -36,7 +53,7 @@ mark_as_advanced(GMP_LIBRARY GMP_INCLUDE_DIR)
 if (GMP_FOUND AND NOT TARGET GMP::SHARED)
     add_library(GMP::SHARED SHARED IMPORTED)
     target_include_directories(GMP::SHARED INTERFACE ${GMP_INCLUDE_DIR})
-    target_link_libraries(GMP::SHARED INTERFACE ${GMP_SHARED_LIBRARY})
+    target_link_libraries(GMP::SHARED INTERFACE ${GMP_C_SHARED_LIBRARY} ${GMP_SHARED_LIBRARY})
     set_target_properties(
             GMP::SHARED
             PROPERTIES
@@ -47,7 +64,7 @@ endif ()
 if (GMP_FOUND AND NOT TARGET GMP::STATIC)
     add_library(GMP::STATIC STATIC IMPORTED)
     target_include_directories(GMP::STATIC INTERFACE ${GMP_INCLUDE_DIR})
-    target_link_libraries(GMP::STATIC INTERFACE ${GMP_STATIC_LIBRARY})
+    target_link_libraries(GMP::STATIC INTERFACE ${GMP_C_STATIC_LIBRARY} ${GMP_STATIC_LIBRARY})
     set_target_properties(
             GMP::STATIC
             PROPERTIES
